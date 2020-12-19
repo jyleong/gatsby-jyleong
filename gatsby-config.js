284bac86d4efcb0aff32010046d7c603a1c991e3
James Leong <james.leong94@gmail.com>
2020-12-19 12:37:55 -0800

    config file str fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,7 +95,7 @@ module.exports = {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: `${process.env.GOOGLE_ANALYTICS_ID}`,
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
